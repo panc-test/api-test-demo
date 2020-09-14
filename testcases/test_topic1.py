a8f8672dd47a2903b155ef0dbc96c195f13ecb04
panchao <chao.pan@dhc.com.cn>
2020-09-14 09:24:27 +0800

    '2020-09-14'

---
+++ testcases/test_topic1.py
@@ -0,0 +1,51 @@
+'''
+主题首页：get
+url:http://49.233.108.117:3000/api/v1/topics
+page Number 页数
+tab String 主题分类。目前有 ask share job good
+limit Number 每一页的主题数量
+mdrender String 当为 false 时，不渲染。默认为 true，渲染出现的所有 markdown 格式文本
+
+'''
+
+import unittest
+import requests
+from ddt import ddt,data
+
+params_data1 = {
+    "page": 1,
+    "tab": "share",
+    "limit": 1,
+    "mdrender": "false"
+}
+
+params_data2 = {
+    "page": 1,
+    "tab": "ask",
+    "limit": 1,
+    "mdrender": "false"
+}
+
+@ddt    #数据驱动
+class TestTopics(unittest.TestCase):
+
+    def setUp(self) -> None:
+        print('start')
+
+    def tearDown(self) -> None:
+        print('end')
+
+    @data(params_data1,params_data2)      #加载测试数据
+    def test_index_page(self,value):
+        r = requests.get(url='http://49.233.108.117:3000/api/v1/topics', params=value)
+        response = r.json()
+        print('response=',response)
+
+        #添加断言
+        self.assertEqual(r.status_code,200,msg='响应状态码=200')
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
+
---
+++ testcases/test_topic3.py
@@ -0,0 +1,44 @@
+'''
+主题首页：get
+url:http://49.233.108.117:3000/api/v1/topics
+page Number 页数
+tab String 主题分类。目前有 ask share job good
+limit Number 每一页的主题数量
+mdrender String 当为 false 时，不渲染。默认为 true，渲染出现的所有 markdown 格式文本
+
+'''
+
+import unittest
+import requests
+from ddt import ddt,file_data
+
+@ddt    #数据驱动
+class TestTopics(unittest.TestCase):
+
+    def setUp(self) -> None:
+        print('start')
+
+    def tearDown(self) -> None:
+        print('end')
+
+    @file_data("../data/topics.yaml")      #加载测试数据
+    def test_index_page(self,page,tab,limit,mdrender):
+        value = {
+            "page" : page,
+            "tab" : tab,
+            "limit" : limit,
+            "mdrender" : mdrender
+        }
+        r = requests.get(url='http://49.233.108.117:3000/api/v1/topics', params=value)
+        response = r.json()
+        print('response=',response)
+
+        #添加断言
+        self.assertEqual(r.status_code,200,msg='响应状态码=200')
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
+
+
