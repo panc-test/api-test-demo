7c8d2f1b4d64519aa6c35551506206c92311ac6f
panchao <chao.pan@dhc.com.cn>
2020-09-21 16:52:28 +0800

    '2020-09-21'

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ def suite():
 
 if __name__ == '__main__':
     suite = suite()
-    result=BeautifulReport(suite)
-    result.report(report_dir='./report',filename='cnode测试报告',description='test_conde')
+    # result=BeautifulReport(suite)
+    # result.report(report_dir='./report',filename='cnode测试报告',description='test_conde')
+    runner=unittest.TextTestRunner()
+    runner.run(suite)
 
 
