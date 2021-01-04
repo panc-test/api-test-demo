ed5b10c554c32e9929fa034f876b21ded3832a08
panc-test <1976085712@qq.com>
2021-01-04 20:08:01 +0800

    2021-01-04

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from BeautifulReport import BeautifulReport
 
 
 def suite():
+    """
+    定义测试套件，并加载测试用例
+    :return:
+    """
     suite=unittest.TestSuite()
     tests=unittest.TestLoader().discover(start_dir='./testcases',pattern="test*.py")
     suite.addTests(tests)
@@ -14,6 +18,9 @@ def suite():
 
 
 if __name__ == '__main__':
+    """
+    执行测试套件，生成测试报告
+    """
     suite = suite()
     result=BeautifulReport(suite)
     result.report(report_dir='./report',filename='cnode测试报告',description='test_20200914')
